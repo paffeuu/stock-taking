6371b3c456f809b8f65b02e4fe257267c4ead8a7
pkowanski <pkowanski@inteca.pl>
2019-12-31 10:54:08 +0100

    Initial commit

---
+++ src/main/java/com/developol/stocktaking/StocktakingApplication.java
@@ -0,0 +1,13 @@
+package com.developol.stocktaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktakingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developol/stocktaking/StocktakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developol.stocktaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
